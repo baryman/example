8ba4503b00fbd4e4bd5558c41e06b48574b04ac6
baryman <bary_man@mail.ru>
2018-02-02 00:30:35 +0300

    Новый бэкбон

--- js/main.js
+++ js/main.js
@@ -1,73 +1,47 @@
+var parentView = Backbone.View.extend({
+  initialize: function(){
+	console.log('parent');
+	this.childMethod();
+	this.childchildMethod();
+	this.parentMethod();
+	console.log(this._a);
+	console.log(this._b);
+  },
+  
+  parentMethod: function(){
+	console.log('parentMethod');
+  }
+});
 
-// Модель человека
-var Person = Backbone.Model.extend({
-	defaults: {
-		name: 'Maxim',
-		age: 20,
-		job: 'Web-developer'
-	}
+var childView = parentView.extend({
+  initialize: function(){
+	this._b = 2;
+	 
+	parentView.prototype.initialize.apply(this);
+  },
+
+  childMethod: function(){
+	console.log('childMethod');
+  }
 });
 
 
-// Список людей
-var PeopleCollection = Backbone.Collection.extend({
-	model: Person
+// У всех дочерних вьюх есть доступ к функциям родителя через this
+// Чтобы передать все функции дочерних вьюх к главной вьюхе
+// Нужно использовать View.prototype.initialize.apply(this);
+var childChildView = childView.extend({
+  initialize: function(){
+	  this._a = 1;	
+	  this.parentMethod();
+	  this.childMethod();
+	  childView.prototype.initialize.apply(this);
+	  this.childMethod();
+	  this.parentMethod();
+  },
+
+  childchildMethod: function(){
+	console.log('childchildMethod');
+  }
 });
 
-
-// Вид представления одного человека
-var PersonView = Backbone.View.extend({
-	initialize: function(){
-		this.render();
-	},
-	tagName: 'li',
-	// 2 способ
-	//template: _.template($('#person-id').html()),
-
-	// 3 способ
-	template: '#person-id',
-	render: function(){
-
-		// 1 Способ (не стоит так делать)
-		//this.$el.html(this.model.get('name') + '('+ this.model.get('age') + ') - ' + this.model.get('job'));
-		
-		// 2 способ
-		//this.$el.html(this.template(this.model.toJSON()));
-
-		// 3 способ
-		var template = _.template($(this.template).html());
-		this.$el.html(template(this.model.toJSON()));
-
-
-	}
-});
-/*
-var person = new Person;
-var personView = new PersonView({model: person});
-
-
-var person2 = new Person({name: 'Andrey', age: 27});
-var personView2 = new PersonView({model: person2});
-*/
-
-var peopleCollection = new PeopleCollection([
-	{
-		name: 'Иван',
-		age: 20,
-		job: 'Таксист'
-	},
-	{
-		name: 'Анна',
-		age: 19,
-		job: 'Студент'
-	},
-	{
-		name: 'Павел',
-		age: 15,
-		job: 'Школьник'
-	}
-]);
-/*
-peopleCollection.add(person);
-peopleCollection.add(person2);
-*/
+new childChildView;
